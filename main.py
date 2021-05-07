73aaff95adbc8f2eecc8ad4abb23c9421e04896a
adrianstando <56126191+adrianstando@users.noreply.github.com>
2021-05-07 18:44:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -304,8 +304,9 @@ def employees(limit: Optional[int] = None, offset: Optional[int] = None, order: 
             order = "id"
         elif order not in ['first_name', 'last_name', 'city', 'id']:
             raise HTTPException(status_code=400)
-        #if limit is None or offset is None:
-        #    raise HTTPException(status_code=400)
+        if limit is None or offset is None:
+            limit = 4
+            offset = 0
 
         connection = sqlite3.connect("northwind.db")
         connection.text_factory = lambda b: b.decode(errors="ignore")
